a7ac340dad3dcadca50ee110b4a74157f4682be4
cshong0618 <cshong.gcc@hotmail.com>
2018-01-28 02:44:45 +0800

    pytorch

--- model.py
+++ model.py
@@ -1,31 +1,74 @@
-from keras.layers import Conv2D, MaxPooling2D, Input, Dense, Flatten, Dropout, UpSampling2D
-from keras.models import Model
-from keras.layers.advanced_activations import LeakyReLU
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torchvision
 
-def generative_model(input_shape=(64, 64, 3), leaky_relu_alpha=0.2):
-    lrelu = LeakyReLU(leaky_relu_alpha)
+class GenerativeModel(nn.Module):
+    def __init__(self, dimen_in, dimen_out):
+        super(GenerativeModel, self).__init__()
+        self.dimen_in = dimen_in
+        self.dimen_out = dimen_out
 
-    model_input = Input(input_shape)
+        self.conv1x16 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=2, stride=1, padding=0),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2)
+        )
 
-    # encoder
-    x = Conv2D(16, (3, 3), activation=lrelu, padding='same')(model_input)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation=lrelu, padding='same')(x)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation=lrelu, padding='same')(x)
-    encoded = MaxPooling2D((2, 2), padding='same')(x)
+        self.conv16x32 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=2, stride=1, padding=0),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2)
+        )
 
-    # decoder
-    x = Conv2D(8, (3, 3), activation=lrelu, padding='same')(encoded)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(8, (3, 3), activation=lrelu, padding='same')(x)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3), activation=lrelu)(x)
-    x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(1, (3, 3), activation='tanh', padding='same')(x)
+        self.conv32x64 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=1, padding=0),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2)
+        )
 
-    # Compile
-    autoencoder = Model(model_input, decoded)
-    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+        self.conv64x64 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, stride=1, padding=0),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2)
+        )
 
-    return autoencoder
+        self.conv64x32 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=2, stride=1, padding=0),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.conv32x16 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=2, stride=1, padding=0),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.conv16x1 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=1, kernel_size=2, stride=1, padding=0),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.conv1x16(x)        # (batch, in_x, in_y, in_z)
+        x = self.conv16x32(x)       # (batch, in_x / 2, in_y / 2, in_z)
+        x = self.conv32x64(x)
+        x = self.conv64x64(x)
+        x = self.conv64x64(x)        
+        x = self.conv64x32(x)
+        x = self.conv32x16(x)
+        x = self.conv16x1(x)
+        x = x.view(x.size(0), -1)
+        
+        size = 1
+        for i, n in enumerate(x.size()):
+            if i > 0:
+                size *= n
+
+        x = nn.Linear(size, 10)
+        return x
+
+_g = GenerativeModel((64, 64, 3), (64, 64, 3))
+print(_g)
+
